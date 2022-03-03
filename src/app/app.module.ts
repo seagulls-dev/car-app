700998ceda7b6235c90b0d6ef211202f081e2040
admin <admin@ckazakov.xyz>
2022-03-03 11:24:15 +0200

    add page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SellCarComponent } from './pages/sell-car/sell-car.component';
+import { BuyCarComponent } from './pages/buy-car/buy-car.component';
+import { MarketComponent } from './pages/market/market.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SellCarComponent,
+    BuyCarComponent,
+    MarketComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
